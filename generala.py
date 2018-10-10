9daed5a44e2aa1db5e3b6de4823302a0a6f8ce2b
Maq301 <studentlorena101@gmail.com>
2018-10-10 19:50:52 -0300

    tester

---
+++ generala.py
@@ -0,0 +1,7 @@
+civil = int(input("Ingrese su estado civil: Si esta casado/a ingrese 1, si es soltero/a ingrese 0: "))
+if (civil == 1):
+    nombre_pareja = input("Ingrese el nombre de su pareja: ")
+    print("Te amo, ", nombre_pareja)
+else:
+    print("Le buscaremos una pareja")
+
